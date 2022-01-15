4a4c1e97ec90c0995c7ed8f643c1aaafba22d167
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-15 13:08:00 +0300

    Added multi-result function.

--- tour/functions.go
+++ tour/functions.go
@@ -6,9 +6,16 @@ func add(a, b int) int {
 	return a + b
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(10, 32))
 
 	resFromClosure := func(a, b int) int { return a * b }(10, 23)
 	fmt.Println(resFromClosure)
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
