a0c1febf82bdab43ab6aa39471e4dc147c4dece2
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-17 13:11:43 +0300

    Added factored var declaration.

--- tour/vars.go
+++ tour/vars.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
 
-var count int
+var (
+	count  int
+	ToBe   bool       = true
+	MaxInt uint64     = 1<<64 - 1
+	root   complex128 = cmplx.Sqrt(-4)
+)
 
 func main() {
 	var c, python, java = true, true, "no!"
 	l := 5
 
 	fmt.Println(count, c, python, java, l)
+
+	fmt.Printf("Type %T, value %v\n", ToBe, ToBe)
+	fmt.Printf("Type %T, value %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type %T, value %v", root, root)
 }
