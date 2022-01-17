50ce7661f7c62775354317944890945027ec030b
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-17 13:42:03 +0300

    Added numeric constants lesson.

--- tour/vars.go
+++ tour/vars.go
@@ -14,6 +14,19 @@ var (
 	root   complex128 = cmplx.Sqrt(-4)
 )
 
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
 func main() {
 	var c, python, java = true, true, "no!"
 	l := 5
@@ -28,4 +41,10 @@ func main() {
 	fmt.Printf("v - %v, q - %q\n", s, s)
 
 	fmt.Println("happy", Pi, "day")
+
+	// Numeric Constants
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+	// fmt.Println(needInt(Big)) // overflow
 }
