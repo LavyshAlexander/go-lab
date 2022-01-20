665151c600fc836e3453ba2263b6133a544ca995
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-20 17:29:46 +0300

    Added remote package cmp.

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,12 @@ package main
 import (
 	"example/user/hello/morestrings"
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println("Hello, world from breezing Go.")
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Printf(cmp.Diff("Hello World", "Hello Go"))
 }
