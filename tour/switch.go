74639715a2ff21fad35c76aed353cc2495950285
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-21 20:29:10 +0300

    Added example with empty switch.

--- tour/switch.go
+++ tour/switch.go
@@ -29,4 +29,15 @@ func main() {
 	default:
 		fmt.Println("Too far away.")
 	}
+
+	// Switch with no condition
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening...")
+	}
 }
