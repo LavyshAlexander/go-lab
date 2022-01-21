4ad77b8204dc53deeffadd450cc899d805de5590
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-21 20:06:36 +0300

    Added debug output.

--- tour/exerciseLoops.go
+++ tour/exerciseLoops.go
@@ -9,13 +9,17 @@ const epsilon float64 = 0.1
 
 func squareRoot(x float64) (z float64) {
 	z = 1
+	iter := 0
+
 	for math.Abs(z*z-x) > epsilon {
+		fmt.Println(iter, z)
 		z -= (z*z - x) / (2 * z)
+		iter += 1
 	}
 
 	return
 }
 
 func main() {
-	fmt.Println(squareRoot(2))
+	fmt.Println(squareRoot(200))
 }
