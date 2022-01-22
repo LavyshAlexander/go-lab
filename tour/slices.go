f9fc0c6124017aec34a3a74ea0aefbc5ef6b7381
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-22 12:42:50 +0300

    Added usage of length and capacity and changing it for slices.

--- tour/slices.go
+++ tour/slices.go
@@ -19,11 +19,23 @@ func main() {
 	fmt.Println(a, b)
 	fmt.Println(names)
 
-	// length and capacity
+	fmt.Println("-- length and capacity --")
 	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	s = s[:0]
+	printSlice(s)
+
+	s = s[:4]
+	printSlice(s)
+
+	s = s[2:]
+	printSlice(s)
+
+	s = s[:4]
 	printSlice(s)
 }
 
 func printSlice(s []int) {
-	panic("unimplemented")
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
