99f57f869014921086d6c2e629d548ee6b417ff5
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-22 12:18:30 +0300

    Added usage of different struct literals.

--- tour/structs.go
+++ tour/structs.go
@@ -7,10 +7,17 @@ type Vertex struct {
 	Y int
 }
 
+var (
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p2 = &Vertex{100, 23}
+)
+
 func main() {
 	v := Vertex{1, 2}
+
 	p := &v
 	p.X = 1e9
 
-	fmt.Println(v)
+	fmt.Println(v, v2, v3, p2)
 }
