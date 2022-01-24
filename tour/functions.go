8e18c6adc36c42dd4d68ba90a785f676376f0bcb
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-24 15:31:47 +0300

    Added closure example.

--- tour/functions.go
+++ tour/functions.go
@@ -27,4 +27,22 @@ func main() {
 
 	mod, div := mod(10, 3)
 	fmt.Println(mod, div)
+
+	fmt.Println("-- Closures --")
+	pos, neg := adder(), adder()
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
