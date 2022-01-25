7131e0f4364989f59ffd7b9bafb6ed60d29ea2da
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-25 11:49:57 +0300

    Added type switch example.

--- tour/typeAssertions.go
+++ tour/typeAssertions.go
@@ -14,6 +14,26 @@ func main() {
 	f, ok := i.(float64)
 	fmt.Println(f, ok)
 
-	f = i.(float64) // panic
-	fmt.Println(f)
+	// f = i.(float64) // panic
+	// fmt.Println(f)
+
+	fmt.Println("-- Type switches --")
+	do(21)
+	a := 42
+	do(&a)
+	do("hello")
+	do(true)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case *int:
+		fmt.Printf("Twice of pointed value %v is %v\n", *v, *v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
 }
