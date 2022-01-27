330ee0a8c2ce568adcdebe955a24099fc86f2589
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-27 13:13:32 +0300

    Added example of embedding structs as composition technique.

---
+++ byExample/embedding.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
+func main() {
+	co := container{
+		base: base{10},
+		str:  "some name",
+	}
+
+	fmt.Println(co)
+	fmt.Println(co.base.num, co.num == co.base.num)
+	fmt.Println(co.describe())
+
+	type describer interface {
+		describe() string
+	}
+
+	var d describer = co
+	fmt.Println(d.describe())
+}
