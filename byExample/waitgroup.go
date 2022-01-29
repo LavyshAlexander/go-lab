7a512b5a34f49f4aee2df04ae3407e67b6c5360f
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-29 10:44:37 +0300

    Added interesting example of wait group.

---
+++ byExample/waitgroup.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+
+		i := i // https://go.dev/doc/faq#closures_and_goroutines
+
+		go func() {
+			worker(i, &wg)
+		}()
+	}
+
+	wg.Wait()
+}
