29332f4dbe82c76e6773e84a5b4dcc2c6ff47490
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-01-31 12:37:49 +0300

    Added mutex example.

---
+++ byExample/mutexes.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type CounterContainer struct {
+	mu       sync.Mutex
+	counters map[string]int
+}
+
+func (c *CounterContainer) inc(name string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.counters[name]++
+}
+
+func main() {
+	c := CounterContainer{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+
+	var wg sync.WaitGroup
+
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			c.inc(name)
+		}
+
+		wg.Done()
+	}
+
+	wg.Add(3)
+	doIncrement("a", 1000)
+	doIncrement("a", 1000)
+	doIncrement("b", 1000)
+
+	wg.Wait()
+	fmt.Println(c.counters)
+}
