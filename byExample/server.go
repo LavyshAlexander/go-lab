5f5e7c8ecb7feab3cf8ecd89629a8b1164990261
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-02-15 19:24:23 +0300

    Added context usage example in server practice.

--- byExample/server.go
+++ byExample/server.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
