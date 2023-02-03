7574d1e48f71114c31643c3758c9a1d71dd04317
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-02-03 15:16:20 +0700

    Added goodbye handler.

--- msa/main.go
+++ msa/main.go
@@ -9,10 +9,13 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbuy", gh)
 
 	http.ListenAndServe(":9090", sm)
 }
